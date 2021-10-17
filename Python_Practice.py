e910d48fd2977d0cf9462d8a66bfa2afd973e7be
Meredith Tracy <meredithtracy@MacBook-Pro.local>
2021-10-17 16:16:55 -0700

    updates to PyPoll doc

--- Python_Practice.py
+++ Python_Practice.py
@@ -1,5 +1,3 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-if "Arapahoe" in counties and "El Paso" not in counties:
-   print("Only Arapahoe is in the list of counties.")
-else:
-    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+numbers = [0, 1, 2, 3, 4]
+for num in numbers:
+    print(num)
